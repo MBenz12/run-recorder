6d0904a863983977ea12a5fcb14bd006573e82ba
mbenz12 <mbenz10120112@gmail.com>
2023-11-02 18:29:46 -0500

    fixed issue #13: remove all .DS_Store files from repo

--- neverstop Watch App/Views/TestView.swift
+++ neverstop Watch App/Views/TestView.swift
@@ -8,8 +8,6 @@
 import SwiftUI
 
 struct TestView: View {
-    @State var text: String = "hello!"
-    
     @GestureState private var isDetectingLongPress = false
     @State private var completedLongPress = false
 
@@ -18,7 +16,7 @@ struct TestView: View {
         LongPressGesture(minimumDuration: 5)
             .updating($isDetectingLongPress) { currentState, gestureState, transaction in
                 gestureState = currentState
-                transaction.animation = Animation.easeInOut(duration: 1)
+                transaction.animation = Animation.easeInOut(duration: 2)
                 self.completedLongPress = false;
             }
             .onEnded { finished in
@@ -27,26 +25,19 @@ struct TestView: View {
     }
     var body: some View {
         VStack {
-            Text(isDetectingLongPress ? "Detecting" : "")
-            Text(completedLongPress ? "Completed" : "")
-            Button(action: {
-                text = "action!"
-            }) {
-                Text("Start")
-                    .frame(width: 100, height: 50, alignment: .center)
-                    .background(Color.blue)
-                    .cornerRadius(15)
-            }
-            .frame(width: 100, height: 50, alignment: .center)
-            .cornerRadius(15)
             Text("Stop")
                 .frame(width: 100, height: 50, alignment: .center)
                 .background(self.isDetectingLongPress ?
                             Color.red :
                             (self.completedLongPress ? Color.green : Color.blue))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(self.completedLongPress ? Color.green : Color.clear, lineWidth: 2)
+                        .padding(2)
+                )
                 .cornerRadius(15)
-                .gesture(longPress)
                 .scaleEffect(isDetectingLongPress ? 0.95 : 1.0)
+                .gesture(longPress)
         }
     }
 }
